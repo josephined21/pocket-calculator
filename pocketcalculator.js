6e821b35bcd02d2510cb7a5b8207adf840bf080c
Josephine Deng <jojod_27@yahoo.com>
2019-01-04 10:45:31 -0500

    worked on display + structure + functions

--- pocketcalculator.js
+++ pocketcalculator.js
@@ -1,5 +1,18 @@
-let value = 0;
+let n;
 
+function display(value){
+  let display = document.getElementById("display");
+  display.innerHTML += value;
+}
+
+let x = 0;
+let y = 0;
+
+//function () {
+
+//}
+
+/*
 function digits(digit) {
   let display = document.getElementById("display");
   display.innerHTML += digit;
@@ -9,36 +22,25 @@ function decimalpoint() {
   display.innerHTML += ".";
 }
 function operators(operator) {
-  /*
-  if(operator == "&times"){
-
-  }
-  else if(operator == "&divide"){
-
-  }
-  else if(operator == "+"){
-
-  }
-  else if(operator == "-"){
-
-  }
-  */
   let display = document.getElementById("display");
   display.innerHTML += "&nbsp" + operator + "&nbsp";
 }
+*/
+
 function equals() {
   let display = document.getElementById("display");
-  display.innerHTML = value;
+  //display.innerHTML = n;
   //
 }
 function percentage() {
-  //value /= 100;
   let display = document.getElementById("display");
-  display.innerHTML = value;
+  //n = Number(display.innerHTML);
+  //n /= 100;
+  //display.innerHTML = ;
 }
 function negation() {
   let display = document.getElementById("display");
-  //
+  // display.innerHTML = ;
 }
 function clear() {
   let display = document.getElementById("display");
