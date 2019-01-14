0d0f71f72b052fd540b5972ba7267af4fd907406
Josephine Deng <jojod_27@yahoo.com>
2019-01-14 01:10:54 -0500

    moved functions to javascript

--- pocketcalculator.js
+++ pocketcalculator.js
@@ -1,82 +1,15 @@
-// ignore the code below
-/*
-let x;
-let y;
-
-function display(value){
-  let display = document.getElementById("display");
-  display.innerHTML += value;
-  x += value;
+function dis(n) {
+  document.getElementById("display").value += n;
 }
-
-function digits(digit) {
-  let display = document.getElementById("display");
-  display.innerHTML += digit;
+function equals(n) {
+  document.getElementById("display").value = eval(n);
 }
-function decimalpoint() {
-  let display = document.getElementById("display");
-  display.innerHTML += ".";
+function percentage(n) {
+  document.getElementById("display").value = n / 100;
 }
-function operators() {
-  let display = document.getElementById("display");
-  display.innerHTML += operator;
+function negation(n) {
+  document.getElementById("display").value = -n;
 }
-
-let x;
-let y;
-
-function addition(x, y) {
-  return x + y;
-}
-function subtraction(x, y) {
-  return x - y;
-}
-function multiplication(x, y) {
-  return x * y;
-}
-function division(x, y) {
-  return x / y;
-}
-function powers2(x) {
-  return x ** 2;
-}
-function powers3(x) {
-  return x ** 3;
-}
-function powers(x, y) {
-  return x ** y;
-}
-function squareroot(x) {
-  return Math.SQRT(x);
-}
-function root(x) {
-  return (x);
-
-function equals() {
-  let display = document.getElementById("display");
-
-if(isNaN(a)){
-    display.innerHTML = "ERROR";
-  }
-
-  display.innerHTML = eval(x);
-}
-
-function percentage() {
-  let display = document.getElementById("display");
-  x /= 100;
-  display.innerHTML = x;
-}
-
-function negation() {
-  let display = document.getElementById("display");
-  x = -x;
-  display.innerHTML = x;
-}
-
-
 function clear() {
-  var display = document.getElementById("display");
-  display.value = 0;
+  document.getElementById("display").value = "0";
 }
-*/
