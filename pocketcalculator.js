81d608672b7ec24d0cf0efb80ac495392059daef
Josephine Deng <jojod_27@yahoo.com>
2019-01-22 12:57:46 -0500

    worked on everything

--- pocketcalculator.js
+++ pocketcalculator.js
@@ -14,21 +14,50 @@ function dis(val) {
 }
 
 // decimal point
-function decimalPoint(val) {
+function getNotDecimalPoint(val) {
   return val !== ".";
 }
 
-//commas
 /*
-function commas(val) {
-  let del = ",";
-  let a = val.split('.', 2);
-  let b = a[1];
-  let c = parseInt(a[0]);
-  if (isNaN(c)) {
-    return '';
+function decimalPoint(val) {
+  let arr1 = [];
+  let str1 = val.toString();
+  for (let i = 0; i < str1.length; i++) {
+    arr1.push(str.charAt(i));
   }
-  return val;
+
+  for (let j = 0; j < arr1.length; j++) {
+    let firstDecimalIndex;
+    let decimalPointPressed = false;
+    while (!isNaN(arr[j]) || arr[j] === ".") {
+      if (arr[j] === ".") {
+        decimalPointPressed = true;
+        firstDecimalIndex = j;
+        break;
+      }
+    }
+  }
+
+  let arr2 = [];
+  let arr3 = [];
+  let arr4 = [];
+  let str2;
+
+  if (decimalPointPressed === true) {
+    for (let k = 0; k < firstDecimalIndex + 1; k++) {
+      arr2.push(arr[k]);
+    }
+    for (let l = firstDecimalIndex + 1; l < arr1.length; l++) {
+      arr3.push(arr[l]);
+    }
+    arr3 = arr3.filter(getNotDecimalPoint);
+    arr4 = arr4.concat(arr1, arr2);
+    str2 = arr4.join("");
+  }
+  else {
+    str2 = arr1.join("");
+  }
+  return str2;
 }
 */
 
@@ -44,66 +73,31 @@ function equals() {
 
   // decimal point
   /*
-  let arr1 = [];
-  let arr2 = [];
-  let arr3 = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    let firstDecimalIndex;
-    let decimalPointPressed = false;
-    while (!isNaN(arr[i]) || arr[i] === ".") {
-      if (arr[j] === ".") {
-        decimalPointPressed = true;
-        firstDecimalIndex = j;
-        break;
+  let str;
+  for (let j = 0; j < arr.length; j++) {
+    if (!isNaN(arr[j])) {
+      while (!isNaN(arr[j]) || arr[j] == ".") {
+        let n;
       }
     }
   }
-
-  let num;
-  let str;
-  if (decimalPointPressed === true) {
-    for (let j = 0; j < firstDecimalIndex + 1; j++) {
-      arr1.push(arr[j]);
-    }
-    for (let k = firstDecimalIndex + 1; k < arr.length; k++) {
-      arr2.push(arr[k]);
-    }
-    arr2 = arr2.filter(decimalPoint);
-    arr3 = arr3.concat(arr1, arr2);
-    str = arr3.join("");
-    num = eval(str);
-  }
-  else {
-    str = arr.join("");
-    num = eval(str);
-  }
   */
+
+  let str = arr.join("");
+  num = eval(str);
 
   // scientific notation
   if (num > 999999999) {
     num = num.toExponential();
   }
 
-  // errors
-  if (num == "NaN" || num == "undefined" || num == "Infinity") {
-    num = "ERROR"
-  }
   // commas
-  else {
-    for (let n = 0; n < arr.length; n++) {
-      if (!isNaN(arr[n])) {
-        let num1;
-        let num2 = 0;
-        while (!isNaN(arr[n])) {
-          num1 += arr[n] * (10 ** num2);
-          num2++;
-          if (num2 == 3) {
-            num1 += ",";
-          }
-        }
-      }
-    }
+  if (!isNaN(num)) {
+    num = num.toLocaleString();
+  }
+  // errors
+  else if (val == "NaN" || val == "undefined" || val == "Infinity") {
+    num = "ERROR";
   }
 
   document.getElementById("display").value = num;
@@ -127,7 +121,6 @@ function ac() {
 
 /*
 to do:
-numbers that exceed 999 should be comma-delimited
 limit input sizes to 999,999,999
-if an operator is entered immediatley after another operator(s), ignore the previous operator(s)
+decimal points afer the first decimal point in a number are ignored
 */
