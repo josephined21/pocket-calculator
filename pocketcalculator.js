096bbb03a06c9b36e46981a9aa70b25213fb05da
Josephine Deng <jojod_27@yahoo.com>
2019-01-22 23:52:17 -0500

    finished

--- pocketcalculator.js
+++ pocketcalculator.js
@@ -14,81 +14,38 @@ function dis(val) {
 }
 
 // display digits
-let numDigitsPressed = 0;
+let digitsPressed = 0;
 function digits(val) {
-  if (numDigitsPressed < 9) {
+  if (digitsPressed < 9) {
     if (arr[0] === 0) {
       arr.shift();
       arr.push(val);
       document.getElementById("display").value = val;
-      numDigitsPressed++;
+      digitsPressed++;
     }
     else {
       arr.push(val);
       document.getElementById("display").value += val;
-      numDigitsPressed++;
+      digitsPressed++;
     }
   }
 }
 
 // display operators
+let operatorsPressed = 0;
 function operators(val) {
-  numDigitsPressed = 0;
+  digitsPressed = 0;
   if (arr[0] === 0) {
     arr.shift();
     arr.push(val);
     document.getElementById("display").value = val;
+    operatorsPressed++;
   }
   else {
     arr.push(val);
     document.getElementById("display").value += val;
+    operatorsPressed++;
   }
-}
-
-// decimal point
-function getNotDecimalPoint(val) {
-  return val !== ".";
-}
-
-function decimalPoint(val) {
-  let arr1 = [];
-  let str1 = val.toString();
-  for (let i = 0; i < str1.length; i++) {
-    arr1.push(str.charAt(i));
-  }
-
-  for (let j = 0; j < arr1.length; j++) {
-    let firstDecimalIndex;
-    let decimalPointPressed = false;
-    while (!isNaN(arr[j]) || arr[j] === ".") {
-      if (arr[j] === ".") {
-        decimalPointPressed = true;
-        firstDecimalIndex = j;
-        break;
-      }
-    }
-  }
-
-  let arr2 = [];
-  let arr3 = [];
-  let arr4 = [];
-  let str2;
-
-  if (decimalPointPressed === true) {
-    for (let k = 0; k < firstDecimalIndex + 1; k++) {
-      arr2.push(arr[k]);
-    }
-    for (let l = firstDecimalIndex + 1; l < arr1.length; l++) {
-      arr3.push(arr[l]);
-    }
-    arr3 = arr3.filter(getNotDecimalPoint);
-    arr4 = arr4.concat(arr1, arr2);
-    str2 = arr4.join("");
-  }
-  else {
-    str2 = arr1.join("");
-  }
-  return str2;
 }
 
 // equals
@@ -105,16 +62,26 @@ function equals(val) {
   for (let j = 0; j < arr.length - 1; j++) {
     if ((arr[j] == "+" || arr[j] == "-" || arr[j] == "*" || arr[j] == "/") && (arr[j + 1] == "+" || arr[j + 1] == "-" || arr[j + 1] == "*" || arr[j + 1] == "/")) {
       arr.splice(j, 1);
+      operatorsPressed--;
       j--;
     }
   }
 
   // decimal point
-  /*
+  let decimalPointPressed;
   for (let k = 0; k < arr.length; k++) {
-
+    decimalPointPressed = false;
+    while (!isNaN(arr[k]) || arr[k] == ".") {
+      if (arr[k] == "." && decimalPointPressed == false) {
+        decimalPointPressed = true;
+      }
+      else if (arr[k] == "." && decimalPointPressed == true) {
+        arr.splice(k, 1);
+        k--;
+      }
+      k++;
+    }
   }
-  */
 
   let str = arr.join("");
   let num = eval(str);
@@ -124,17 +91,21 @@ function equals(val) {
     num = num.toExponential();
   }
 
-  // commas
-  if (!isNaN(num)) {
-    num = num.toLocaleString();
-  }
   // errors
   if (num == "NaN" || num == "undefined" || num == "Infinity") {
     arr = [0];
     num = "ERROR";
   }
 
-  document.getElementById("display").value = num;
+  digitsPressed = 0;
+  for (let l = 0; l < arr.length; l++) {
+    if (!isNaN(arr[l])) {
+      digitsPressed++;
+    }
+  }
+  operatorsPressed = 0;
+
+  document.getElementById("display").value = num.toLocaleString();
 }
 
 // percentage
@@ -149,12 +120,8 @@ function negation(val) {
 
 // clear
 function ac() {
-  numDigitsPressed = 0;
+  digitsPressed = 0;
+  operatorsPressed = 0;
   arr = [0];
   document.getElementById("display").value = "0";
 }
-
-/*
-to do:
-decimal points afer the first decimal point in a number are ignored
-*/
